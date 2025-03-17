d31ddc2bb810a4b55a2f7ee42fa439d487039a23
hdao21 <huuduy.dao@dxc.com>
2025-03-17 09:21:48 +0000

    first commit

---
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
+using Mango.Services.OrderAPI.Utility;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mango.MessageBus;
+using Microsoft.EntityFrameworkCore;
+using Mango.Services.OrderAPI.RabbmitMQSender;
+
+namespace Mango.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        private IProductService _productService;
+        private readonly IRabbmitMQOrderMessageSender _messageBus;
+        private readonly IConfiguration _configuration;
+        public OrderAPIController(AppDbContext db,
+            IProductService productService, IMapper mapper, IConfiguration configuration
+            , IRabbmitMQOrderMessageSender messageBus)
+        {
+            _db = db;
+            _messageBus = messageBus;
+            this._response = new ResponseDto();
+            _productService = productService;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrders")]
+        public ResponseDto? Get(string? userId = "")
+        {
+            try
+            {
+                IEnumerable<OrderHeader> objList;
+                if (User.IsInRole(SD.RoleAdmin))
+                {
+                    objList = _db.OrderHeaders.Include(u => u.OrderDetails).OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+                else
+                {
+                    objList = _db.OrderHeaders.Include(u => u.OrderDetails).Where(u=>u.UserId==userId).OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+                _response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(objList);
+            }
+            catch (Exception ex) 
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrder/{id:int}")]
+        public ResponseDto? Get(int id)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails).First(u => u.OrderHeaderId == id);
+                _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+
+
+        [Authorize]
+        [HttpPost("CreateOrder")]
+        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = SD.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+
+                OrderHeader orderCreated = _db.OrderHeaders.Add(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _db.SaveChangesAsync();
+
+                orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+                _response.Result = orderHeaderDto;
+
+                RewardsDto rewardsDto = new()
+                {
+                    OrderId = orderHeaderDto.OrderHeaderId,
+                    RewardsActivity = Convert.ToInt32(orderHeaderDto.OrderTotal),
+                    UserId = orderHeaderDto.UserId
+                };
+                string topicName = _configuration.GetValue<string>("TopicAndQueueNames:OrderCreatedTopic");
+                _messageBus.SendMessage(rewardsDto, topicName);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message=ex.Message;
+            }
+            return _response;
+        }     
+
+        [Authorize]
+        [HttpPost("UpdateOrderStatus/{orderId:int}")]
+        public async Task<ResponseDto> UpdateOrderStatus(int orderId, [FromBody] string newStatus)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.First(u => u.OrderHeaderId == orderId);
+                if (orderHeader != null)
+                {
+                    orderHeader.Status = newStatus;
+                    _db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+    }
+}
